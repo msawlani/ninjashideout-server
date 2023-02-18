e03555a747e350d7476260e66a2f280e678fbf7b
Michael <39811434+msawlani@users.noreply.github.com>
2023-02-18 01:29:41 -0500

    updated router
    able to delete one and update one document by the name instead of id

--- commandsRouter.js
+++ commandsRouter.js
@@ -27,22 +27,22 @@ commandRouter
   });
 
 commandRouter
-  .route("/:id")
+  .route("/:command")
   .delete((req, res, next) => {
-    Commands.findByIdAndRemove(req.params.id).then((resp) => {
-      console.log(req.params.id);
+    Commands.deleteOne({ command: req.params.command }).then((resp) => {
+      console.log(req.params.command);
       res.status = 200;
       res.set("Content-Type", "application/json");
       res.json(resp);
     });
   })
   .put((req, res, next) => {
-    Commands.findByIdAndUpdate(
-      req.params.id,
+    Commands.updateOne(
+      { command: req.params.command },
       { $set: req.body },
       { new: true }
     ).then((resp) => {
-      console.log(req.params.id);
+      console.log(req.params.command);
       res.status = 200;
       res.setHeader("Content-Type", "application/json");
       res.json(resp);
